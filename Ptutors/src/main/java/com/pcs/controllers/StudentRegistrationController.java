b274d541b24ef8a23de7656e63879255e97d6b3b
Govind Kumar Choudhary <govind.contact@gmail.com>
2018-11-16 00:10:08 +0530

    commit koley in govind branch

--- Ptutors/src/main/java/com/pcs/controllers/StudentRegistrationController.java
+++ Ptutors/src/main/java/com/pcs/controllers/StudentRegistrationController.java
@@ -12,7 +12,7 @@ import com.pcs.service.StudentRegistrationService;
 @Controller
 public class StudentRegistrationController {
 	
-	// Mriganka
+	// Mriganka Koley
 
 	@Autowired
 	StudentRegistrationService srs;
